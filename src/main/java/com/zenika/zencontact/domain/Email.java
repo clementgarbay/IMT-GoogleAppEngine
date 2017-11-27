303a5d9902aeaa80bdaba599df5c32585851a393
Clément Garbay <clementgarbay@gmail.com>
2017-11-27 11:59:04 +0100

    Implement email sender

---
+++ src/main/java/com/zenika/zencontact/domain/Email.java
@@ -0,0 +1,11 @@
+package com.zenika.zencontact.domain;
+
+/**
+ * @author Clément Garbay
+ */
+public class Email {
+    public String subject;
+    public String body;
+    public String to;
+    public String toName;
+}
